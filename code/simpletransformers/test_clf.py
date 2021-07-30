991e8d2fb9b7f9662526fe7133c4432101441719
Shelly Wang <c656wang@uwaterloo.ca>
2021-07-29 22:04:12 -0400

    Change test_clf to use a shared function

--- code/simpletransformers/test_clf.py
+++ code/simpletransformers/test_clf.py
@@ -9,35 +9,15 @@ from sklearn.metrics import precision_recall_fscore_support
 from simpletransformers.classification import (ClassificationModel, ClassificationArgs)
 from clf_utils import *
 
+
 def test_trained_clf(args):
     
     clf_folder = os.path.join(args.classifier, args.clf_best_model_folder)
     model = ClassificationModel(args.model, clf_folder, use_cuda=args.use_cuda)
-    
-    clf_test_data = []
-    
-    for i,fpath in enumerate(args.data):
-        data = open(fpath, 'r').readlines()
-        data = [l.strip() for l in data]
-        data = [l.split(args.delimiter) for l in data]
-        
-        # classifying pairs: format [src, tgt, label]; each file has a unique label
-        if args.pairs:
-            clf_test_data += [[s,t,i] for (s,t) in data]
-        
-        # classifying src to tgt: format [src, tgt]
-        else:
-            clf_test_data += [[s, label_names[t]] for (s,t) in data]
-    
-    test_df = pd.DataFrame(clf_test_data)
-    
-    if args.pairs:
-        test_df.columns = ["text_a", "text_b", "labels"]
-        tgt = [i for (s, t, i) in clf_test_data]
-    else:
-        test_df.columns = ["text", "labels"]
-        tgt = [t for (s,t) in clf_test_data]
-    
+
+    # prepare all test data and label
+    test_df, tgt = prepare_clf_data(args.classifier, args.data, args.pairs, args.delimiter)
+
     result, outputs, wrong_predictions = model.eval_model(test_df)
     preds = [list(l).index(max(l)) for l in outputs]
     precision, recall, f1, support = precision_recall_fscore_support(y_true=tgt, y_pred=preds, average='macro', zero_division=0)
