4ad77f4beafb2900a2ebc2cc22dbbeafb4e35cc2
YujiaG <yujia.guo@aalto.fi>
2021-11-08 00:42:04 +0200

    first

--- reasoning-over-facts/scripts/run_language_modeling.py
+++ reasoning-over-facts/scripts/run_language_modeling.py
@@ -212,7 +212,11 @@ def train(args, train_dataset, corrects, model: PreTrainedModel, tokenizer: PreT
                 steps_trained_in_current_epoch -= 1
                 continue
 
+
             inputs, labels = mask_tokens(batch, tokenizer, args)
+            print('inputs ', inputs[:100])
+            print('labels ', labels[:100])
+
             inputs = inputs.to(args.device)
             labels = labels.to(args.device)
             model.train()
